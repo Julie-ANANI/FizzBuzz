dd8b430c5e06dffd9c1685788ee1f2962af51ef8
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2024-02-19 12:25:25 +0100

    Initial commit
    add file .gitignore and start config

---
+++ src/fizzbuzz.spec.ts
@@ -0,0 +1,15 @@
+import { fizzBuzz } from "./fizzbuzz";
+
+describe('FizzBuzz', () => {
+    test(' should returns 1 when 1 in as input', () => {
+        // Arrange
+        // les starts data
+
+        // Act
+        // execute an mutation
+
+        // Assert
+        //test result
+
+    });
+});
